c1f48b7e8d175973a1d6635a216fc995a25b05dd
jhjhjhlll88 <167087417+jhjhjhlll88@users.noreply.github.com>
2025-04-19 19:00:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ def predict():
         })
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Use the port Render assigns
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=3000)
+
